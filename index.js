9d9cd80a49c3d8320911bf2e025706b1afdff1a1
F <f@dxw.com>
2019-02-20 17:38:20 +0000

    Use the real API to get rent data
    Still uses a dummy tenancy agreement ID.

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = new alexa.app("hackney-tenant");
 
 app.launch(async (_req, res) => {
   const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/users/1"
+    "https://g6bw0g0ojk.execute-api.eu-west-2.amazonaws.com/production/ncc/api/UH/GetAllRentBreakdowns?tenancyAgreementId=000015%2F01",
+    { headers: { "x-api-key": process.env.HACKNEY_API_KEY } }
   );
 
-  res.say(response.data.name);
+  res.say(response.data[0].description);
 });
 
 exports.handler = app.lambda();
